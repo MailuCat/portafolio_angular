b1ace39bbece95bcada94b74d41a266d22dd5117
MailuCat <f.tramites@gmail.com>
2022-07-20 19:00:41 -0400

    tres

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PortadaComponent } from './portada/portada.component';
 import { MistrabajosComponent } from './mistrabajos/mistrabajos.component';
 import { ContactoComponent } from './contacto/contacto.component';
 
 const routes: Routes = [
-  { path:'portada' , component: PortadaComponent },
   { path:'mistrabajos' , component: MistrabajosComponent },
   { path:'contacto' , component: ContactoComponent },
    
